c541299444594cecf47e5a00d484c0cfa93da854
Galina Atamankina <g.atamankina@gmail.com>
2024-09-04 11:14:00 +0200

    Add basic app structure.

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.post('/feedback', (req, res) => {
+    const { title, text } = req.body;
+
+});
+
+app.get('/feedback', (req, res) => {
+
+});
+
+app.delete('/feedback', (req, res) => {
+
+});
+
+
+app.listen(PORT, ()=> {
+    console.log(`Server laeuft auf http://localhost:${PORT}`);
+});
