13e53fb124b5bf0f1a18fc20d478e19249348a86
Galina Atamankina <g.atamankina@gmail.com>
2024-09-05 10:05:56 +0200

    Moving the data storage to the data folder. Fixing the delete /feedback logic.

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ app.use(express.json());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const feedbackFilePath = path.join(__dirname, 'feedback.json');
+const feedbackDirPath = path.join(__dirname, 'data');
+const feedbackFilePath = path.join(feedbackDirPath, 'feedback.json');
 
 // Helper functions
 const loadFeedback = async () => {
@@ -62,21 +63,21 @@ app.get('/feedback', async (req, res) => {
 });
 
 app.delete('/feedback/:title', async (req, res) => {
-    const title = req.params;
+    const { title } = req.params;
 
     try {
         let feedback = await loadFeedback();
         const filteredFeedback = feedback.filter(fb => fb.title !== title);
 
         if (feedback.length === filteredFeedback.length ) {
-            return res.status(404).json({ message: "Feedback nicht gefunden. " });
+            return res.status(404).json({ message: "Feedback nicht gefunden." });
         }
 
         await saveFeedback(filteredFeedback);
         res.status(200).json({ message: "Feedback erfolgreich geloescht." });
 
     } catch (error) {
-        res.status(500).json({ message: 'Fehler beim Loeschen des Feedbacks.' });
+        res.status(500).json({ message: "Fehler beim Loeschen des Feedbacks." });
     }
 
 });
