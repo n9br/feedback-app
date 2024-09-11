2a07def51fcf2d7dfadca47c01b14e0ac0318d55
Galina Atamankina <g.atamankina@gmail.com>
2024-09-11 13:11:33 +0200

    Fixing the userdata script.

--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 #Install Docker
-sudo yum update -y
-sudo yum install -y docker
+yum update -y
+yum install -y docker
 
 #Start Docker
-sudo service docker start
-sudo systemctl enable docker
+service docker start
+systemctl enable docker
 
 #Install Docker-Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.0.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/v2.0.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 #Download Docker-Compose App Config
+mkdir /home/ec2-user/feedback-app
+cd /home/ec2-user/feedback-app
 wget https://raw.githubusercontent.com/atamankina/feedback-app/main/docker-compose.yml
 
 #Start the app
-sudo docker-compose up -d
+docker-compose up -d
